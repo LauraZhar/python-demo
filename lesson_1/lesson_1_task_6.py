f8b3b90e3f68612be4b0cd465c6a8a13571f7fac
LauraZhar <doskalievalaura97@gmail.com>
2024-04-22 01:41:00 +0600

    lesson1

---
+++ lesson_1/lesson_1_task_6.py
@@ -0,0 +1,56 @@
+def print_8():
+    print(8, end="")
+
+
+def print_8_0():
+    print(8, end="")
+    print(0, end="")
+
+
+def print_5():
+    print(5, end="")
+
+
+def print_5_5():
+    print(5, end="")
+    print(5, end="")
+
+
+def print_3():
+    print(3, end="")
+
+
+def print_3_5():
+    print(3, end="")
+    print(5, end="")
+
+
+def print_0():
+    print(0, end="")
+
+
+def print_8_8():
+    print(8, end="")
+    print(8, end="")
+
+
+def print_0_0():
+    print(0, end="")
+    print(0, end="")
+
+
+def print_8_5_5():
+    print(8, end="")
+    print(5, end="")
+    print(5, end="")
+
+
+print_8_8()
+print_0_0()
+print_5_5()
+print_5()
+print_3_5()
+print_3()
+print_8()
+print_0()
+print_8_5_5()
